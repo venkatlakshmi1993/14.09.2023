481735e7d3674d1a14c03baf5867731e378d7673
venkatlakshmikiot@gmail.com <venkatlakshmikiot@gmail.com>
2023-09-15 13:04:08 +0530

    adding sample file

---
+++ Sample.java
@@ -0,0 +1,23 @@
+package org.newproject;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sample {
+	
+	public static void main(String[] args){
+		
+	  List<String> al = new LinkedList<String>();
+ 
+        al.add("Sachin");
+        al.add("Rahul");
+        al.add("gow"); 
+
+
+       System.out.println();
+    }
+
+	}
+
